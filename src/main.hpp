8aad18ac9a396b58c1bb398d96553b7f8c6c1f50
iqrok <blogo3x@gmail.com>
2023-07-13 11:40:45 +0700

    Added routine with sleep loop for comparation

--- src/main.hpp
+++ src/main.hpp
@@ -17,31 +17,34 @@ using namespace std;
 
 namespace MainRoutine {
 
-const uint64_t period_ns_io = 500'000;
+constexpr int schedule_priority = SCHED_RR;
 
-const uint64_t offset_ns = 7'500;
-//~ const uint64_t offset_ns = 10000;
+const uint64_t period_ns_busy = 500'000;
+const uint64_t period_ns_sleep = 500'000;
+
+const uint64_t offset_ns = 0;
 const uint32_t max_sample_size = 5'000;
-
-std::vector<double> sample_io;
-std::vector<double> diff_sample_io;
 
 bool isRunning = false;
 
-std::thread thrd_io;
+std::vector<double> sample_sleep;
+std::vector<double> sample_busy;
 
-void routine()
+std::thread thrd_sleep;
+std::thread thrd_busy;
+
+void routine_sleep()
 {
 	pid_t tid = gettid();
 
 #if DEBUG > 0
-	printf("Starting Ventilation IO thread %d\n", tid);
+	printf("Starting Sleep Loop thread %d\n", tid);
 #endif
 
 	struct sched_param param = {};
-	param.sched_priority = sched_get_priority_max(SCHED_FIFO);
+	param.sched_priority = sched_get_priority_max(schedule_priority);
 
-	if (sched_setscheduler(tid, SCHED_FIFO, &param) == -1) {
+	if (sched_setscheduler(tid, schedule_priority, &param) == -1) {
 		perror("sched_setscheduler failed");
 	}
 
@@ -49,7 +52,7 @@ void routine()
 
 	struct timespec wakeup_time;
 	uint32_t index = 0;
-	uint64_t period_ns = period_ns_io - offset_ns;
+	uint64_t period_ns = period_ns_sleep - offset_ns;
 
 	while (isRunning) {
 		Sleep::start_timer(&wakeup_time);
@@ -57,29 +60,63 @@ void routine()
 
 		double x = 1000 * 0.1234325325;
 
-		//~ uint64_t exec_time = Sleep::wait_for_given_period(wakeup_time, period_ns);
-		uint64_t exec_time = Sleep::busy_wait_for_given_period(wakeup_time, period_ns);
+		uint64_t exec_time = Sleep::wait(wakeup_time, period_ns);
 		uint64_t pause_time = Timestamp::now_ns() - time_start;
-		//~ printf(" %ld\n", pause_time);
 
-		Statistics::push(&sample_io, pause_time, &index, max_sample_size);
-		Statistics::push(&diff_sample_io, exec_time, &index, max_sample_size);
+		Statistics::push(&sample_sleep, pause_time, &index, max_sample_size);
+	}
+}
+
+void routine_busy()
+{
+	pid_t tid = gettid();
+
+#if DEBUG > 0
+	printf("Starting Busy Loop thread %d\n", tid);
+#endif
+
+	struct sched_param param = {};
+	param.sched_priority = sched_get_priority_max(schedule_priority);
+
+	if (sched_setscheduler(tid, schedule_priority, &param) == -1) {
+		perror("sched_setscheduler failed");
+	}
+
+	setpriority(PRIO_PROCESS, tid, -1);
+
+	struct timespec wakeup_time;
+	uint32_t index = 0;
+	uint64_t period_ns = period_ns_busy - offset_ns;
+
+	while (isRunning) {
+		Sleep::start_timer(&wakeup_time);
+		uint64_t time_start = Timestamp::now_ns();
+
+		double x = 1000 * 0.1234325325;
+
+		uint64_t exec_time = Sleep::busy_wait(wakeup_time, period_ns);
+		uint64_t pause_time = Timestamp::now_ns() - time_start;
+
+		Statistics::push(&sample_busy, pause_time, &index, max_sample_size);
 	}
 }
 
 void start()
 {
 	isRunning = true;
-	thrd_io = std::thread(&routine);
+	thrd_sleep = std::thread(&routine_sleep);
+	thrd_busy = std::thread(&routine_busy);
 }
 
 void stop()
 {
 	isRunning = false;
-	thrd_io.join();
 
-	Statistics::print_stats(sample_io, "IO & Control loop", true);
-	Statistics::print_stats(diff_sample_io, "IO & Control exec time", false);
+	thrd_sleep.join();
+	thrd_busy.join();
+
+	Statistics::print_stats(sample_sleep, "Sleep loop", true);
+	Statistics::print_stats(sample_busy, "Busy loop", false);
 }
 
 }
