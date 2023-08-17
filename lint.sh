dbaa21998306962c0f9634a5cc10d03217880a8f
iqrok-flex <blogo3x@gmail.com>
2023-08-17 20:15:42 +0700

    Update lint.sh

--- lint.sh
+++ lint.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
 DIR=$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )
-FILE_LIST="$(find "${DIR}/src" \
+FILE_LIST="$(find "${DIR}" \
 	| grep -v "${DIR}/build" \
 	| grep -E ".*(\.ino|\.cpp|\.c|\.h|\.hpp|\.hh)$"
 )"
